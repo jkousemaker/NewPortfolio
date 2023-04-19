d766e1f8191c6bd001f1d717683eeac3f2efec8a
jkousemaker <j.l.kousemaker@gmail.com>
2023-04-19 23:33:43 +0200

    Added scrollable sections

--- responsiveMargin.js
+++ responsiveMargin.js
@@ -1,7 +1,5 @@
-const header = document.querySelector("header");
 const aside = document.querySelector("aside");
 const main = document.querySelector("main");
-const bottom = document.querySelector(".c-bottom");
 
 function update() {
   // Get the computed style of the aside element
@@ -10,13 +8,9 @@ function update() {
   // Check if the position property of the computed style is set to "fixed"
   if (asideStyle.position === "fixed") {
     // Set the padding top and bottom of the aside element to its height
-    aside.style.marginTop = header.offsetHeight + "px";
-    aside.style.marginBottom = bottom.offsetHeight + "px";
     main.style.marginInline = aside.offsetWidth + "px";
   } else {
     // Reset the padding top and bottom of the aside element
-    aside.style.marginTop = 0;
-    aside.style.marginBottom = 0;
     main.style.marginInline = '';
   }
 }
--- sectionSwitch.js
+++ sectionSwitch.js
@@ -1,31 +1,62 @@
 const sections = document.querySelectorAll("section");
 const navButtons = document.querySelectorAll(".nav-link");
 
-let running = false;
-
 document.querySelector("main").addEventListener('wheel', function(event)
 {
-    let currentSectionId = getVisibleSection().id;
-    let currentSectionNumber = currentSectionId.match(/(\d+)/)[0];
-    let nextSectionId;
-    let nextSectionNumber;
+    if (event.deltaY < 0) handleSwitch("up");
+    else if (event.deltaY > 0) handleSwitch("down");
     
-    if (event.deltaY < 0){
-        nextSectionNumber = parseInt(currentSectionNumber) - 1;
-        nextSectionId = "section" + nextSectionNumber;
-        switchSection(nextSectionId);
-    }
-    else if (event.deltaY > 0){
-        nextSectionNumber = parseInt(currentSectionNumber) + 1;
-        nextSectionId = "section" + nextSectionNumber;
-        switchSection(nextSectionId);     
-    }
 });
 
-function switchSection(id) {
-    if(running || !getVisibleSection() || !getNextSection(id)) {
-        return;
+// Add event listeners for touch events
+let touchstartY = 0;
+let touchendY = 0;
+
+window.addEventListener('touchstart', (event) => {
+    touchstartY = event.touches[0].clientY;
+});
+
+window.addEventListener('touchend', (event) => {
+    touchendY = event.changedTouches[0].clientY;
+    const swipedDistance = touchendY - touchstartY;
+    const minSwipeDistance = 50;
+    
+    // Check swipe direction and trigger handleSwitch function
+    if (swipedDistance < -minSwipeDistance) handleSwitch("down");
+    else if (swipedDistance > minSwipeDistance) handleSwitch("up");
+});
+
+let scrolled = 0;
+let timer;
+
+function handleSwitch(direction) {
+    let currentSection = getVisibleSection();
+    let currentSectionId = currentSection.id;
+    let nextSectionId;
+    let nextSectionNumber;
+
+    let completedScroll = false;
+    
+    //Checks if element has been scrolled all the way and sets next section number based on direction of scroll
+    if (direction == "up" && currentSection.scrollTop === 0){
+        nextSectionNumber = parseInt(currentSectionId.match(/(\d+)/)[0]) - 1;
+        completedScroll = true;
     }
+    else if (direction == "down" && currentSection.scrollTop + currentSection.clientHeight >= currentSection.scrollHeight){
+        nextSectionNumber = parseInt(currentSectionId.match(/(\d+)/)[0]) + 1;
+        completedScroll = true;
+    }
+
+    nextSectionId = "section" + nextSectionNumber;
+
+    if(completedScroll) switchSection(nextSectionId);    
+}
+
+
+let running = false;
+
+function switchSection(id) {
+    if(running || !getVisibleSection() || !getNextSection(id)) return;
     running = true;
 
     const currentSection = getVisibleSection();
@@ -44,32 +75,46 @@ function switchSection(id) {
      });
      
      sectionTimeLine
+     //Puts next section into position for animation
      .add( {
         targets: nextSection,
         duration: 0,
         translateY: nextTransform
-     })
+     }, 0)
+     //Transforms current section out of viewport
      .add( {
         targets: currentSection,
         translateY: currentTransform,
-        easing: 'easeInOutQuad'
+        easing: 'easeInOutQuad',
      })
      .add( {
         targets: nextSection,
         translateY: 'calc(0vh + 0px)',
-        easing: 'easeInOutQuad'
+        easing: 'easeInOutQuad',
      }, '-='+duration*0.8)
-     
 
      setTimeout(() => {
         currentSection.classList.add("hidden");
 
-        if(nextSection.id.match(/(\d+)/)[0] == sections.length) {
-            anime({
-                targets: 'footer',
-                height: '3rem'
-            });
+        let footerHeight;
+        let easing;
+        let duration;
+        if(checkLastSection(nextSection)) {
+            footerHeight = "3rem";
+            easing = "easeOutBack";
+            duration = 500;
+        } else {
+            footerHeight = "0";
+            easing = "easeOutCubic";
+            duration = 300;
         }
+
+        anime( {
+            targets: 'footer',
+            height: footerHeight,
+            easing: easing,
+            duration: duration
+        })
 
         running = false;
      }, duration)
@@ -79,6 +124,7 @@ function switchSection(id) {
 function getVisibleSection() {
     let style;
 
+    //Loops through all the sections to check which one is visible 
     for(let i = 0; i < sections.length; i++) {
         style = getComputedStyle(sections[i]);
         if(style.display != "none") {
@@ -90,12 +136,15 @@ function getVisibleSection() {
 }
 
 function getNextSection(id) {
+
+    //Loops through all the sections to check which one is the next section
     for(let i = 0; i < sections.length; i++) {
         if(sections[i].id == id) {
             return sections[i];
         }
     }
 
+    //Returns false if section doesn't exist preventing errors
     return false;
 }
 
@@ -106,6 +155,8 @@ function getTransforms(currentSection, nextSection) {
     let nextTransform;
     let currentTransform;
 
+    //Checks if current section is before or after the next section and saves transform values in variables
+    //Values make sure transform will be out of viewport
     if(currentSection.id.match(/(\d+)/)[0] < nextSection.id.match(/(\d+)/)[0]) {
         nextTransform = "calc(100vh - "+headerHeight+"px)";
         currentTransform = "calc(-100vh + "+footerHeight+"px)";
@@ -115,4 +166,14 @@ function getTransforms(currentSection, nextSection) {
     }
 
     return [nextTransform, currentTransform];
+}
+
+function checkLastSection(nextSection) {
+
+    //Checks if nextSection is last section
+    if(nextSection.id.match(/(\d+)/)[0] == sections.length) {
+        return true;
+    }
+
+    return false;
 }
